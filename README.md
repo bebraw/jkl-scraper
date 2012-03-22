1ef6f4a3253fa8714a6075448d0fd2503d854310
Juho Vepsalainen <bebraw@gmail.com>
2012-03-22 19:21:44 +0200

    Get meeting links

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Scrapes certain council data and outputs JSON.
 
+## TODO
+
+* Scrape, scrape, scrape
+* Write a batch converter for files (they're iso-8859-1). Use iconv to make them utf-8.
+
--- server.js
+++ server.js
@@ -3,5 +3,12 @@
 var path = require('path');
 
 var connect = require('connect');
-connect.createServer(connect.static(path.join(__dirname, 'Demo')), {encoding: 'utf-8'}).listen(8080);
+connect.createServer(connect.static(
+            path.join(__dirname, 'Demo'),
+            {maxAge: 0},
+            function(req, res) {
+                res.setHeader('Content-Type', 'text/html');
+                res.setHeader('Accept-Charset', 'iso-8859-1');
+            }
+            )).listen(8080);
 
